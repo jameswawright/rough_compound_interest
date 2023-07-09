55f794b92a8e268909946b27e4c54030b73d7aab
James Wright <126224464+jameswawright@users.noreply.github.com>
2023-07-09 17:23:54 +0100

    Add files via upload

---
+++ self_finance.py
@@ -0,0 +1,15 @@
+from numpy import random
+
+inv = 30000
+month_inv = 500
+
+for i in range(1,30):
+    for j in range(1,12):
+        
+        if random.uniform(0, 1) > 0.10:
+            r = random.normal(0.07, 0.025)
+        else:
+            r = random.normal(-0.05, 0.025)
+        inv = inv*(1+r/12) + month_inv
+
+print(round(inv,2))
